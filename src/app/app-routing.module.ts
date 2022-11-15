2e030608b2a2506248a1f2ad02c05684442da415
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-11-15 17:23:18 -0300

    Redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: '**', redirectTo: ''}
+  {path: ':id',  component: HomeComponent}
 ];
 
 @NgModule({
