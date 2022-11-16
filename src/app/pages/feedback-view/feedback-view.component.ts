84ab11d48d955cc2a2584464bd2614eb52b428a7
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-11-16 10:48:17 -0300

    Feat gestion de activar

--- src/app/pages/feedback-view/feedback-view.component.ts
+++ src/app/pages/feedback-view/feedback-view.component.ts
@@ -8,30 +8,37 @@ import { StorageService } from 'src/app/services/storage.service';
   styleUrls: ['./feedback-view.component.scss']
 })
 export class FeedbackViewComponent implements OnInit {
-  feedback:any;
+  feedback: any;
   loading: boolean = true;
-  event:any;
+  event: any;
   getId = this.router.url.split('/')[2].trim();
 
-
-  constructor(private router:Router, private storageSvc:StorageService) { }
+  constructor(private router: Router, private storageSvc: StorageService) {}
 
   ngOnInit(): void {
     this.getFeedback();
   }
 
-  getFeedback(){
+  getFeedback() {
     this.loading = true;
-    this.storageSvc.GetByParameter('feedbacks', 'id', this.getId).subscribe((res:any)=>{
+    this.storageSvc.GetByParameter('feedbacks', 'id', this.getId).subscribe((res: any) => {
       this.feedback = res;
       console.log(this.feedback);
+    });
 
-    })
-
-    this.storageSvc.GetByParameter('events', 'id', this.getId).subscribe((res:any)=>{
+    this.storageSvc.GetByParameter('events', 'id', this.getId).subscribe((res: any) => {
       this.event = res[0];
       this.loading = false;
     });
   }
 
+  toggleSesion() {
+    if (this.event.active) {
+      this.event.active = false;
+      this.storageSvc.Update(this.getId, 'events', this.event).then(() => {});
+    } else {
+      this.event.active = true;
+      this.storageSvc.Update(this.getId, 'events', this.event).then(() => {});
+    }
+  }
 }
