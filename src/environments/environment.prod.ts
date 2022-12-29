5da4221c1e69ee21c7bdac6ae7ac77dbc32461d0
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-12-28 23:33:00 -0300

    Refactor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,6 @@ export const environment = {
     messagingSenderId: "999491895502",
     appId: "1:999491895502:web:0420933fc256df01e5d09f",
     measurementId: "G-6RN7Z3S6N9"
-  }
+  },
+  url_deploy: 'http://feedbackcito.com.ar/#/'
 };
